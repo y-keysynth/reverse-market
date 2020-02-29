95e6322d20e6cb5a6911a3a0649951ebc2403e83
y-keysynth <ykhojojp@gmail.com>
2020-02-29 17:58:11 +0900

    ベーシック認証の解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
-  before_action :basic_auth, if: :production? 
-  protect_from_forgery with: :exception
+  # before_action :basic_auth, if: :production? 
+  # protect_from_forgery with: :exception
 
 def set_search
   @search = Item.ransack(params[:q])
@@ -17,15 +17,15 @@ end
   end
 
   private
-  def production?
-    Rails.env.production?
-  end
+  # def production?
+  #   Rails.env.production?
+  # end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
+  # end
 
   protected
   # 入力フォームからアカウント名情報をDBに保存するために追加
